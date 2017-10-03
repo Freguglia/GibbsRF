2b226d7787a50d5fd001469e2a71378b679fd527
Victor Freguglia <victorfreguglia@gmail.com>
2017-10-03 16:49:58 -0300

    Finished variable selection functions.

--- R/betaPLS.R
+++ R/betaPLS.R
@@ -8,7 +8,7 @@
 #' @author Victor Freguglia Souza
 #'
 
-betaPLS = function(coeftab,lambda,penalty.type="L0",fnlambda = function(a,b) exp(max(a,b)/2)){
+betaPLS = function(coeftab,lambda,penalty.type="L0",fnlambda = function(b1,b2) exp(max(b1,b2)/2),a=3.7){
   coeftab = coeftab %>% rowwise() %>% mutate(lambdas = lambda*fnlambda(n,m))
   if(penalty.type=="L0"){
     coeftab = coeftab %>% mutate(coefs = coefs*(abs(coefs)>lambdas)) %>%
@@ -23,6 +23,19 @@ betaPLS = function(coeftab,lambda,penalty.type="L0",fnlambda = function(a,b) exp
   }
 
   if(penalty.type=="SCAD"){
+    SCAD = function(b,lambda){
+      num = length(b)
+
+      ifelse(abs(b)<=(2*lambda),
+             max(0,(abs(b) - lambda))*sign(b),
+             ifelse(
+               (((2*lambda) < abs(b)) && (abs(b) <= a*lambda )),
+               ((a-1)*b - sign(b)*a*lambda)/(a-2) ,
+                    b))
+    }
+    coeftab = coeftab %>% rowwise() %>% mutate(coefs = SCAD(coefs,lambdas)) %>%
+      filter(abs(coefs)>0)
+    return(coeftab)
 
   }
   message("Penalty type not valid.")
