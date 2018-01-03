04dfc2562cd8590414e114f1a328b6f577d5bca5
VicFreguglia <victorfreguglia@gmail.com>
2018-01-03 19:53:55 -0200

    More flexible cMatPlot function.

--- R/cMat_plot.R
+++ R/cMat_plot.R
@@ -7,10 +7,11 @@ cMatPlot = function(cMat){
   x = complete_cMat(cMat)[,1]
   y = complete_cMat(cMat)[,2]
   df = data.frame(x,y)
-  p = ggplot(df,aes(x=x,y=y,xmin=-5,xmax=5)) +
+  maxw = max(abs(df$x),abs(df$y)) + 0.5
+  p = ggplot(df,aes(x=x,y=y,xmin=-maxw,xmax=maxw)) +
     geom_tile(fill="gray90",colour="black") +
-    scale_x_continuous(limits=c(-5,5)) +
-    scale_y_continuous(limits=c(-5,5)) +
+    scale_x_continuous(limits=c(-maxw,maxw)) +
+    scale_y_continuous(limits=c(-maxw,maxw)) +
     theme_void() +
     annotate("text", x = 0, y = 0, label = "i")
   plot(p)
